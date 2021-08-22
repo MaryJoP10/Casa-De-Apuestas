d510dd32a2f42453d82a46eb2fe46272deb3b43d
MaryJoP10 <maryjop10@gmail.com>
2021-08-21 21:59:26 -0500

    Adelanto

---
+++ src/casa_apuestas/Casa_apuestas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package casa_apuestas;
+
+/**
+ *
+ * @author ASUS
+ */
+import frame.Apuestas_frame;
+public class Casa_apuestas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Apuestas_frame apu= new Apuestas_frame();
+        apu.setVisible(true);
+    }
+    
+}
